ceef17f97e3c1373f0ce8af3811ff5d89197ff83
fodych <beloysov.egor@nttek.ru>
2024-06-05 16:18:50 +0500

    button

---
+++ app/Orchid/Screens/SuppliesScreen.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Orchid\Screens;
+
+use App\Models\Supply;
+use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Layout;
+
+class SuppliesScreen extends Screen
+{
+    /**
+     * Fetch data to be displayed on the screen.
+     *
+     * @return array
+     */
+    public function query(): iterable
+    {
+        return [
+            'supplies' => Supply::paginate(10),
+        ];
+    }
+
+    /**
+     * The name of the screen displayed in the header.
+     *
+     * @return string|null
+     */
+    public function name(): ?string
+    {
+        return 'Товары';
+    }
+
+    /**
+     * The screen's action buttons.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
+    public function commandBar(): array
+    {
+        return [
+            \Orchid\Screen\Actions\Link::make('Создать товар')
+                ->icon('bs.plus')
+                ->route('platform.supply'),
+
+        ];
+    }
+
+    /**
+     * The screen's layout elements.
+     *
+     * @return \Orchid\Screen\Layout[]|string[]
+     */
+    public function layout(): iterable
+    {
+        return [
+            Layout::table('supplies',[
+
+            ])
+        ];
+    }
+}
